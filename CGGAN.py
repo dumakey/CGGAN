fixed problem on generator vanishing gradients; discriminator model modified too

CGGAN.py
@@ -248,14 +248,15 @@ class CGGAN:
 
         # Parameters
         case_ID = self.parameters.analysis['case_ID']
-        image_shape = (self.parameters.img_size[1],self.parameters.img_size[0],1)
+        image_shape = (self.parameters.img_size[1],self.parameters.img_size[0],1)  # (height, width, channels)
         noise_dim = self.parameters.training_parameters['noise_dim']
         alpha = self.parameters.training_parameters['learning_rate']
         nepoch = self.parameters.training_parameters['epochs']
         epoch_iter = self.parameters.training_parameters['epoch_iter']
         num_iter = nepoch * epoch_iter
         batch_size = self.parameters.training_parameters['batch_size']
-        batch_shape = (batch_size,image_shape[1],image_shape[0],1)
+        batch_shape = (batch_size,*image_shape)
+        sample_shape = (1,*image_shape)
         l3_reg = self.parameters.training_parameters['l3_reg']
         l2_reg = self.parameters.training_parameters['l2_reg']
         l1_reg = self.parameters.training_parameters['l1_reg']
@@ -299,9 +300,9 @@ class CGGAN:
 
             # Models and functions declaration
             discriminator = self.model.Model[i].Discriminator = models.Discriminator(activation,l2_reg,l1_reg,dropout)
-            generator = self.model.Model[i].Generator = models.Generator(noise_dim,activation,l2_reg,l1_reg,dropout)
+            generator = self.model.Model[i].Generator = models.Generator(image_shape[:2],activation,l2_reg,l1_reg,dropout)
             disc_optimizer = self.model.Optimizers[i].disc_optimizer = models.optimizer(alpha)
-            gen_optimizer = self.model.Optimizers[i].gen_optimizer = models.optimizer(10*alpha)
+            gen_optimizer = self.model.Optimizers[i].gen_optimizer = models.optimizer(alpha)
             loss = models.loss_function
             metric_disc = models.performance_metric
             metric_gen = models.performance_metric
@@ -316,41 +317,37 @@ class CGGAN:
             gen_streaming_metric = 0
             gen_streaming_metric_cv = 0
 
-            '''
+
             (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
-            x_train = x_train.astype("float32")/255.0
-            X_train = np.zeros((x_train.shape[0],image_shape[0],image_shape[1]),dtype=np.float32)
-            for j,img in enumerate(x_train):
-                X_train[j] = cv.resize(img,(image_shape[0],image_shape[1]),interpolation=cv.INTER_AREA)
+            X_train = dataset_processing.preprocess_image(x_train,(image_shape[1],image_shape[0]))
+            X_train = X_train.astype('float32')/255.
             dataset_train = tf.data.Dataset.from_tensor_slices(X_train)
             dataset_train = dataset_train.shuffle(buffer_size=1024).batch(batch_size)
 
-            x_test = dataset_processing.preprocess_image(x_test,(image_shape[0],image_shape[1]))
-            x_test = x_test.astype("float32")/255.0
-            X_test = np.zeros((x_test.shape[0],image_shape[0],image_shape[1]),dtype=np.float32)
-            for j,img in enumerate(x_test):
-                X_test[j] = cv.resize(img,(image_shape[0],image_shape[1]),interpolation=cv.INTER_AREA)
+            X_test = dataset_processing.preprocess_image(x_test,(image_shape[1],image_shape[0]))
+            X_test = X_test.astype('float32')/255.
             dataset_test = tf.data.Dataset.from_tensor_slices(X_test)
             dataset_test = dataset_test.shuffle(buffer_size=1024).batch(1)
             train_iterator = iter(dataset_train)
-            '''
+
 
             # Create iterator
-            train_iterator = iter(self.datasets.dataset_train)
-            for j in range(1,num_iter + 1):
+            #train_iterator = iter(self.datasets.dataset_train)
+            for j in range(0,num_iter):
                 ### Update discriminator
                 with tf.GradientTape() as tape_disc:
                     real_image_batch = tf.reshape(train_iterator.get_next(),batch_shape)
                     noise_batch = tf.random.normal((batch_size,noise_dim))
-                    fake_image_batch = generator(noise_batch) * 0.05*tf.random.normal(batch_shape)
+                    fake_image_batch = generator(noise_batch)
                     # Prediction computations
                     fake_logit_batch = discriminator(fake_image_batch)
                     real_logit_batch = discriminator(real_image_batch)
                     # Ground truth labels
-                    fake_label_batch = tf.zeros((batch_size,1))
-                    real_label_batch = tf.ones((batch_size,1))
+                    fake_label_batch = tf.zeros((batch_size,1)) + 0.05 * tf.random.uniform((batch_size,1))
+                    real_label_batch = tf.ones((batch_size,1)) + 0.05 * tf.random.uniform((batch_size,1))
                     # Loss computation
-                    fake_loss_batch = loss('disc',discriminator,fake_logit_batch,real_logit_batch,fake_image_batch,real_image_batch,l3_reg)
+                    fake_loss_batch = loss('disc',discriminator,fake_logit_batch,fake_label_batch,real_logit_batch,
+                                           real_label_batch,fake_image_batch,real_image_batch,l3_reg)
                     disc_loss_batch = fake_loss_batch + sum(discriminator.losses)
                     # Metric computation
                     fake_metric_batch = metric_disc(fake_logit_batch,fake_label_batch).numpy()
@@ -363,10 +360,10 @@ class CGGAN:
                 ### Update generator
                 with tf.GradientTape() as tape_gen:
                     noise_batch = tf.random.normal((batch_size,noise_dim))
-                    fake_image_batch = generator(noise_batch) * 0.05*tf.random.normal(batch_shape)
+                    fake_image_batch = generator(noise_batch)
                     fake_logit_batch = discriminator(fake_image_batch)
                     fake_label_batch = tf.ones((batch_size,1))
-                    gen_loss_batch = loss('gen',None,fake_logit_batch,None,None,None,0.0) + sum(generator.losses)
+                    gen_loss_batch = loss('gen',None,fake_logit_batch,fake_label_batch,None,None,None,None,0.0) + sum(generator.losses)
                     gen_metric_batch = metric_disc(fake_logit_batch,fake_label_batch).numpy()
                 gen_gradients = tape_gen.gradient(gen_loss_batch,generator.trainable_weights)
                 gen_optimizer.apply_gradients(zip(gen_gradients,generator.trainable_weights))
@@ -387,14 +384,13 @@ class CGGAN:
                     self.model.History[i].gen_loss_train[epoch] = gen_streaming_loss/epoch_iter
                     self.model.History[i].gen_metric_train[epoch] = gen_streaming_metric/epoch_iter
                     # Evaluate on cross-validation dataset
-                    input_shape = (1,image_shape[1],image_shape[0],1)
                     niter = 0
-                    #cv_iterator = iter(dataset_test)
-                    cv_iterator = iter(self.datasets.dataset_cv) # create iterator for cross-validation dataset
+                    cv_iterator = iter(dataset_test)
+                    #cv_iterator = iter(self.datasets.dataset_cv) # create iterator for cross-validation dataset
                     while True:
                         try:
                             ## Evaluate discriminator
-                            real_image_cv = tf.reshape(cv_iterator.get_next(),input_shape)
+                            real_image_cv = tf.reshape(cv_iterator.get_next(),sample_shape)
                             noise_cv = tf.random.normal((1,noise_dim))
                             fake_image_cv = generator(noise_cv)
                             # Prediction computations
@@ -404,9 +400,7 @@ class CGGAN:
                             fake_label_cv = tf.zeros((1,1))
                             real_label_cv = tf.ones((1,1))
                             # Loss computation
-                            fake_loss_cv = loss('disc',discriminator,fake_logit_cv,fake_label_cv,fake_image_cv,real_image_cv,0.0)
-                            real_loss_cv = loss('disc',discriminator,real_logit_cv,real_label_cv,fake_image_cv,real_image_cv,0.0)
-                            disc_loss_cv = 0.5 * (real_loss_cv + fake_loss_cv)
+                            disc_loss_cv = loss('disc',discriminator,fake_logit_cv,fake_label_cv,real_logit_cv,real_label_cv,fake_image_cv,real_image_cv,0.0)
                             # Metric computation
                             fake_metric_cv = metric_disc(fake_logit_cv,fake_label_cv).numpy()
                             real_metric_cv = metric_disc(real_logit_cv,real_label_cv).numpy()
@@ -417,7 +411,7 @@ class CGGAN:
                             fake_image_cv = generator(noise_cv)
                             fake_logit_cv = discriminator(fake_image_cv)
                             fake_label_cv = tf.ones((1,1))
-                            gen_loss_cv = loss('gen',None,fake_logit_batch,fake_label_cv,None,None,0.0)
+                            gen_loss_cv = loss('gen',None,fake_logit_batch,fake_label_cv,None,None,None,None,0.0)
                             gen_metric_cv = metric_disc(fake_logit_cv,fake_label_cv).numpy()
 
                             disc_streaming_loss_cv += disc_loss_cv
@@ -595,11 +589,11 @@ class CGGAN:
                     else:
                         results_folder = os.path.join(results_folder,'{}={:.3f}'.format(sens_var[0],sens_var[1][i]))
                     os.mkdir(results_folder)
-                    disc_loss_plot_filename = 'Discriminator_loss_evolution_{}_{}={}.png'.format(str(case_ID),sens_var[0],str(sens_var[1][i]))
-                    gen_loss_plot_filename = 'Generator_loss_evolution_{}_{}={}.png'.format(str(case_ID),sens_var[0],str(sens_var[1][i]))
+                    disc_loss_plot_filename = 'Discriminator_performance_evolution_{}_{}={}.png'.format(str(case_ID),sens_var[0],str(sens_var[1][i]))
+                    gen_loss_plot_filename = 'Generator_performance_evolution_{}_{}={}.png'.format(str(case_ID),sens_var[0],str(sens_var[1][i]))
                 else:
-                    disc_loss_plot_filename = 'Discriminator_loss_evolution_{}.png'.format(str(case_ID))
-                    gen_loss_plot_filename = 'Generator_loss_evolution_{}.png'.format(str(case_ID))
+                    disc_loss_plot_filename = 'Discriminator_performance_evolution_{}.png'.format(str(case_ID))
+                    gen_loss_plot_filename = 'Generator_performance_evolution_{}.png'.format(str(case_ID))
 
                 fig_disc.savefig(os.path.join(results_folder,disc_loss_plot_filename),dpi=200)
                 fig_gen.savefig(os.path.join(results_folder,gen_loss_plot_filename),dpi=200)

dataset_processing.py
@@ -40,13 +40,14 @@ def read_dataset(case_folder, dataset_folder='Training', format='png'):
 def preprocess_image(imgs, new_dims):
 
     m = len(imgs)
-    imgs_processed = np.zeros((m,new_dims[1],new_dims[0]),dtype=np.float32)
+    width, height = new_dims
+    imgs_processed = np.zeros((m,height,width),dtype=np.float32)
     for i in range(m):
-        if imgs[i].shape[0:2] != (new_dims[1],new_dims[0]):
-            img_processed = ImageTransformer.resize(imgs[i],new_dims)
+        if imgs[i].shape[0:2] != (height,width):
+            img_processed = ImageTransformer.resize(imgs[i],(width,height))
         else:
             img_processed = imgs[i]
-        imgs_processed[i] = cv.bitwise_not(img_processed)
+        imgs_processed[i,:] = cv.bitwise_not(img_processed)
 
     return imgs_processed
 

models.py
@@ -2,41 +2,21 @@ import os
 import numpy as np
 import tensorflow as tf
 
+def loss_function(model, discriminator, fake_logit, fake_label, real_logit, real_label, fake, real, reg):
 
-def swish(x, beta=1):
-
-    return x * tf.keras.backend.sigmoid(beta * x)
-
-def sigmoid(x):
-
-    return 1 / (1 + np.exp(-x))
-
-def loss_function(model, discriminator, fake_logit, real_logit, fake, real, reg):
-
-    #loss = tf.keras.losses.BinaryCrossentropy()(fake_logit,real_label)
+    cost_function = tf.keras.losses.BinaryCrossentropy()
     if model == 'disc':
-        loss = -tf.math.reduce_mean(real_logit-fake_logit)
+        loss = 0.5*(cost_function(fake_logit,fake_label) + cost_function(real_logit,real_label))
+        #loss = -tf.math.reduce_mean(real_logit-fake_logit)
         # Mode collapse regularization penalty
         discriminator_gradient = get_gradient(discriminator,real,fake)
         mode_collapse_reg = gradient_penalty(discriminator_gradient)
     elif model == 'gen':
-        loss = -tf.math.reduce_mean(fake_logit)
+        loss = cost_function(fake_logit,fake_label)
+        #loss = -tf.math.reduce_mean(fake_logit)
         mode_collapse_reg = tf.constant(0.0)
 
     return loss + reg * mode_collapse_reg
-
-def get_weights(input):
-
-    weights_sq_sum = 0
-    weights_abs_sum = 0
-    for weight in input.trainable_weights:
-        weight_np = weight.numpy()
-        weight_np = weight_np.reshape(np.prod(weight_np.shape))
-        for item in weight_np:
-            weights_sq_sum += item**2
-            weights_abs_sum += abs(item)
-
-    return weights_sq_sum, weights_abs_sum
 
 def optimizer(alpha):
 
@@ -110,17 +90,19 @@ class Conv2D_block(tf.keras.Model):
             self.l1_reg = 0.0
             dropout = 0.0
             activation = 'relu'
-        num_channels = num_channels
-        f = kernel_size
-        s = stride
-        p = padding
 
-        if p != 0:
-            self.Padding = tf.keras.layers.ZeroPadding2D(p)
-        else:
-            self.Padding = tf.keras.layers.Activation(None)
-        self.Conv2D = tf.keras.layers.Conv2D(num_channels,kernel_size=f,strides=s,padding='valid',kernel_initializer='glorot_normal',
-                                     kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1_reg,l2=self.l2_reg))
+        # Apply padding
+        if padding == 'same':
+            self.Padding = tf.keras.layers.ZeroPadding2D(0)
+            self.Conv2D = tf.keras.layers.Conv2D(num_channels,kernel_size,stride,padding='same',use_bias=True,
+                                                 kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1_reg,l2=self.l2_reg),
+                                                 kernel_initializer='glorot_normal')
+        elif type(padding) == int:
+            self.Padding = tf.keras.layers.ZeroPadding2D(padding)
+            self.Conv2D = tf.keras.layers.Conv2D(num_channels,kernel_size,stride,padding='valid',use_bias=True,
+                                                 kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1_reg,l2=self.l2_reg),
+                                                 kernel_initializer='glorot_normal')
+
         self.BatchNorm = tf.keras.layers.BatchNormalization()
 
         if activation == 'leakyrelu':
@@ -165,10 +147,8 @@ class Conv2DTranspose_block(tf.keras.Model):
             self.l1_reg = 0.0
             dropout = parameters['dropout']
             activation = 'relu'
-        f = kernel_size
-        s = stride
 
-        self.Conv2DTranspose = tf.keras.layers.Conv2DTranspose(num_channels,kernel_size=f,strides=s,padding='same',
+        self.Conv2DTranspose = tf.keras.layers.Conv2DTranspose(num_channels,kernel_size,stride,padding='same',
                                                                kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1_reg,l2=self.l2_reg),
                                                                kernel_initializer='glorot_normal')
         self.BatchNorm = tf.keras.layers.BatchNormalization()
@@ -235,29 +215,26 @@ class Discriminator(tf.keras.Model):
         self.l2 = l2_reg
         self.dropout = dropout
 
-        self.Conv2D_1 = Conv2D_block(num_channels=17,kernel_size=3,padding=1,stride=1,
+        self.Conv2D_1 = Conv2D_block(num_channels=64,kernel_size=3,padding='same',stride=2,
                                      kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
-        self.Pool_1 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)
-        self.Conv2D_2 = Conv2D_block(num_channels=39,kernel_size=3,padding=1,stride=1,
+        self.Conv2D_2 = Conv2D_block(num_channels=128,kernel_size=3,padding='same',stride=2,
                                      kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
-        self.Pool_2 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)
-        self.Conv2D_3 = Conv2D_block(num_channels=87,kernel_size=3,padding=1,stride=1,
-                                     kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
-        self.Pool_3 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)
-        self.Flatten = tf.keras.layers.Flatten()
-        self.Dense_1 = Dense_layer(512,activation,l1_reg,l2_reg,dropout)
-        self.Dense_2 = tf.keras.layers.Dense(units=1,activation='sigmoid',kernel_regularizer=tf.keras.regularizers.L1L2(l1=l1_reg,l2=l2_reg))
+        self.Pool = tf.keras.layers.GlobalMaxPool2D()
+        self.Dense_1 = Dense_layer(64,activation,l1_reg,l2_reg,dropout)
+        self.Dense_2 = Dense_layer(1,'sigmoid',l1_reg,l2_reg,0.0)
 
         self.model = {
         'Conv2D': [
-                   self.Conv2D_1,
-                   self.Conv2D_2,
-                   self.Conv2D_3,
-                  ],
+        self.Conv2D_1,
+        self.Conv2D_2,
+        ],
         'Dense':[
         self.Dense_1,
+        self.Dense_2,
         ],
-        'Final': [self.Dense_2]
+        'Pool': [
+        self.Pool,
+        ],
         }
     def set_up_CV_state(self):
 
@@ -316,59 +293,45 @@ class Discriminator(tf.keras.Model):
     def __call__(self, X):
 
         net = self.Conv2D_1(X)
-        net = self.Pool_1(net)
         net = self.Conv2D_2(net)
-        net = self.Pool_2(net)
-        net = self.Conv2D_3(net)
-        net = self.Pool_3(net)
-        net = self.Flatten(net)
+        net = self.Pool(net)
         net = self.Dense_1(net)
         net = self.Dense_2(net)
 
         return net
 
 class Generator(tf.keras.Model):
-    def __init__(self, noise_dim, activation, l2_reg, l1_reg, dropout):
+    def __init__(self, input_dim, activation, l2_reg, l1_reg, dropout):
         super(Generator,self).__init__()
         self.l1 = l1_reg
         self.l2 = l2_reg
         self.dropout = dropout
 
-        filt_in = 128
-        f = 5
+        filt_in = 64
+        f = 3
         s = 2
-        self.Dense_1 = Dense_layer(noise_dim*noise_dim*filt_in,activation,l1_reg,l2_reg,dropout)
-        self.Reshape = tf.keras.layers.Reshape((noise_dim,noise_dim,filt_in))
-        self.UpSampling = tf.keras.layers.UpSampling2D()
+        fh = int(input_dim[0]/(2*s))
+        fw = int(input_dim[1]/(2*s))
+        fc = 4
+        self.Dense = Dense_layer(fh*fw*fc,activation,l1_reg,l2_reg,dropout)
+        self.Reshape = tf.keras.layers.Reshape((fh,fw,fc))
         self.Conv2DTranspose_1 = Conv2DTranspose_block(num_channels=filt_in,kernel_size=f,stride=s,
                                                        kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
         self.Conv2DTranspose_2 = Conv2DTranspose_block(num_channels=filt_in//2,kernel_size=f,stride=s,
                                                        kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
-        self.Conv2DTranspose_3 = Conv2DTranspose_block(num_channels=filt_in//4,kernel_size=f,stride=s,
-                                                       kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
-        self.Conv2DTranspose_4 = Conv2DTranspose_block(num_channels=16,kernel_size=f,stride=s,
-                                                       kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
-        self.Conv2DTranspose_5 = Conv2DTranspose_block(num_channels=8,kernel_size=f,stride=s,
-                                                       kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':activation})
-        self.Conv2D = Conv2D_block(num_channels=1,kernel_size=3,padding=1,stride=2,
+        self.Conv2D = Conv2D_block(num_channels=1,kernel_size=7,padding='same',stride=1,
                                    kwargs={'l2_reg':l2_reg,'l1_reg':l1_reg,'dropout':dropout,'activation':'sigmoid'})
 
         self.model = {
         'Conv2DTranspose': [
         self.Conv2DTranspose_1,
         self.Conv2DTranspose_2,
-        self.Conv2DTranspose_3,
-        #self.Conv2DTranspose_4,
-        self.Conv2DTranspose_5,
         ],
         'Conv2D':[
         self.Conv2D,
         ],
         'Dense':[
-        self.Dense_1,
-        ],
-        'UpSampling': [
-        self.UpSampling,
+        self.Dense,
         ],
         'Reshape': [self.Reshape],
         }
@@ -441,29 +404,10 @@ class Generator(tf.keras.Model):
 
     def __call__(self, X):
 
-        '''
-        net = tf.keras.layers.Dense(units=7*7*128,activation=None,kernel_initializer='glorot_normal',
-                                    kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1,l2=self.l2))(X)
-        net = tf.keras.layers.LeakyReLU(0.2)(net)
-        net = tf.keras.layers.Reshape((7,7,128))(net)
-        net = tf.keras.layers.Conv2DTranspose(128,kernel_size=4,strides=2,padding='same',
-                                                               kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1,l2=self.l2),
-                                                               kernel_initializer='glorot_normal')(net)
-        net = tf.keras.layers.LeakyReLU(0.2)(net)
-        net = tf.keras.layers.Conv2DTranspose(128,kernel_size=4,strides=2,padding='same',
-                                                               kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1,l2=self.l2),
-                                                               kernel_initializer='glorot_normal')(net)
-        net = tf.keras.layers.LeakyReLU(0.2)(net)
-        net = tf.keras.layers.Conv2D(1,kernel_size=7,strides=2,padding='same',kernel_initializer='glorot_normal',
-                               kernel_regularizer=tf.keras.regularizers.L1L2(l1=self.l1,l2=self.l2),activation='sigmoid')(net)
-        '''
-        net = self.Dense_1(X)
+        net = self.Dense(X)
         net = self.Reshape(net)
         net = self.Conv2DTranspose_1(net)
         net = self.Conv2DTranspose_2(net)
-        net = self.Conv2DTranspose_3(net)
-        #net = self.Conv2DTranspose_4(net)
-        net = self.Conv2DTranspose_5(net)
         net = self.Conv2D(net)
 
         return net